81aba0be253dc80d442f802d10936d493c92e69a
AbdAllah <abdallah.a.elfatah@gmail.com>
2020-05-06 03:02:06 +0200

    Added LinkedInConst and the abstract UseCase to be used with various use cases

---
+++ linkedinsdk/src/main/java/com/AbdAllahAbdElFattah13/domain/utils/LinkedInConst.kt
@@ -0,0 +1,17 @@
+package com.AbdAllahAbdElFattah13.domain.utils
+
+object LinkedInConst {
+    const val AUTHORIZATION_URL = "https://www.linkedin.com/uas/oauth2/authorization"
+    const val ACCESS_TOKEN_URL = "https://www.linkedin.com/uas/oauth2/accessToken"
+    const val SECRET_KEY_PARAM = "client_secret"
+    const val RESPONSE_TYPE_PARAM = "response_type"
+    const val GRANT_TYPE_PARAM = "grant_type"
+    const val GRANT_TYPE = "authorization_code"
+    const val RESPONSE_TYPE_VALUE = "code"
+    const val CLIENT_ID_PARAM = "client_id"
+    const val STATE_PARAM = "state"
+    const val REDIRECT_URI_PARAM = "redirect_uri"
+    const val QUESTION_MARK = "?"
+    const val AMPERSAND = "&"
+    const val EQUALS = "="
+}
